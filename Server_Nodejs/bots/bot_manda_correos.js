4a9919e000f0012e30ef6f3e94dbb5e281956d37
Caslab <Caslab>
2022-05-20 08:28:58 +0200

    Bot manda correos puede enviar correos sin contexto

--- Server_Nodejs/bots/bot_manda_correos.js
+++ Server_Nodejs/bots/bot_manda_correos.js
@@ -1,43 +1,51 @@
 //  Usar el paquete
+const conexion = require('../config/conexion_bd.config');
 var nodemailer = require('nodemailer');
-var debug = require('../comandos/globales')
-var correo = require('../comandos/mirar_correos')
-
+var debug = require('../comandos/globales');
+var log = require('../comandos/ver_logs')
 /*TODO forma temporal de mandar correo cuando sucede un error se tiene que pasar a la bd
 PARA LA LLAMADA SE NECESITARA CONSULTAR LA BASE DE DATOS Y COMPROBAR QUE TODOS LOS CAMPOS SE PUEDEN RELLENAR
 
-mandarCorreo(llamar_correo(json_correos))
 */
-function mandarCorreo(){
+const profile = require('../usuario.json')
+
+async function mandarCorreo(){
+
+//  Petición al servidor para obtener los correos
+let res_correos = await conexion.query("SELECT * FROM mirar_correos()")
+// El json al estar compuesto con 2 niveles le indicamos el 1
+// ya que en el se encuentran los correos
+var res = res_correos.rows[0].jresultado
+
 //Creamos el objeto de que mandara el correo
 var transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
-    user: '@gmail.com',
-    pass: ''
+    user: profile.user,
+    pass: profile.pass
   }
 })
 
 //  Cuerpo del mensaje enviante
 var mailOptions = {
-    from: 'jnyqrwuazuy@scpulse.com',
-    to: correo(),
+    from: profile.user,
+    to: res[1].correo,
     subject: 'Error de servidor',
-    text: 'prueba de correo electronico'
+    text: 'ejemplo de texto'
 };
 
 //  Simple comando para mandar el correo
 transporter.sendMail(mailOptions, function(error, info){
+  
     if (error) {
-      console.log(error);
+      debug.msg(error);
     } else {
-      console.log('Email enviado: ' + info.response);
+      debug.msg('Email enviado: ' + info.response);
     }
+    transporter.close();
   });
-
+debug.msg("mensaje enviado "+ mailOptions)
 }
 
-//mandarCorreo('dakcans','nixabik662@dakcans.com',' ','georgig200@gmail.com','pruebamensajes')
-//correo(mandarCorreo)
 
 module.exports = mandarCorreo;
