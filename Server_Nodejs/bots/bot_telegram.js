5c9a44466ef6aeb48398cb82bbbb2d97aed2a594
Caslab <Caslab>
2022-05-20 08:30:21 +0200

    Funcion Telegra Asyncrono, NO FUNCIONA

--- Server_Nodejs/bots/bot_telegram.js
+++ Server_Nodejs/bots/bot_telegram.js
@@ -1,31 +1,32 @@
 const TelegramBot = require('node-telegram-bot-api');
 var debug = require('../comandos/globales')
+var logs = require('../comandos/ver_logs')
 
-function botTelegram(mensaje){
+async function botTelegram(x){
+
 // replace the value below with the Telegram token you receive from @BotFather
 const token = '5376651327:AAEkV6M9BYI-FgubYtYQjxSnJuZQyxUQqwM';
 
 // Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(token, {polling: true});
 
-// Matches "/echo [whatever]"
+//Matches "/echo [whatever]"
 bot.onText(/\/echo (.+)/, (msg, match) => {
-  
 
   const chatId = msg.chat.id;
+  const resp = match[1]; // the captured "whatever"
 
   // send back the matched "whatever" to the chat
-  bot.sendMessage(chatId, mensaje);
-
+  bot.sendMessage(chatId, resp);
 });
 
 // Listen for any kind of message. There are different kinds of
 // messages.
 bot.on('message', (msg) => {
   const chatId = msg.chat.id;
-  const resp = match[1]; // the captured "whatever"
+  bot.sendMessage(chatId, x);
+  debug.msg("MENSAJE DE TELEGRAM "+logs)
   // send a message to the chat acknowledging receipt of their message
-  bot.sendMessage(chatId, resp);
 });
 
 }
