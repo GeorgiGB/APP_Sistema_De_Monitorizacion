82f9033584c72d209f7a0571b4fd00b56f9ed74b
Caslab <Caslab>
2022-05-20 08:29:43 +0200

    Ver Logs necesita comprobacion

--- Server_Nodejs/comandos/ver_logs.js
+++ Server_Nodejs/comandos/ver_logs.js
@@ -1,11 +1,14 @@
 const conexion = require('../config/conexion_bd.config');
 const debug = require('./globales');
 
-async function ver_logs(){
+async function ver_logs(json_logs){
+    
+    debug.msg(JSON.stringify(json_logs))
 
-    let res_logs = await conexion.query("SELECT * FROM ver_logs()")
+    let res_logs = await conexion.query("SELECT * FROM ver_logs('"+JSON.stringify(json_logs)+"');");
+    //si el estado esta vacio, mandara los registros del dia actual
 
-    //debug.msg(res_correos.rows[0])
+    debug.msg(res_logs.rows[0])
     return res_logs.rows[0].jresultado;
 }
 
