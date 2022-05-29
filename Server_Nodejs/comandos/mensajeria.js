6bcbcd32a090b11c2294bb847164d2c447bbee14
jbnr360 <joan_navarro@siguetusenda.com>
2022-05-29 22:08:41 +0200

    Se guardan los mensajes no enviados en la tabla  mensajes_no_enviados

--- Server_Nodejs/comandos/mensajeria.js
+++ Server_Nodejs/comandos/mensajeria.js
@@ -6,7 +6,8 @@ const inserta_log = require('./inserta_log');
 var nodemailer = require('nodemailer');//NPM para mandar correos
 const conexion = require('../config/db.config.js');
 const tokenBot = require('../config/tokenBot.json');
-const usuCorreo = require('../config/correo.json');const TelegramBot = require('node-telegram-bot-api');
+const usuCorreo = require('../config/correo.json');
+const TelegramBot = require('node-telegram-bot-api');
 const { Estados } = require('./acciones');
 
 
@@ -22,9 +23,6 @@ async function usuarios_mensajeria(){
             lista_mensajeria.push(usuarios[i])
         }
     }
-    
-    //  Creación de un nuevo archivo en el que se guardaran los usuarios_mensajeria
-    // globales.crearJSon('usuarios_mensajeria',JSON.stringify(usuarios))
 
     return lista_mensajeria;
 }
@@ -36,21 +34,27 @@ async function ver_logs(json_logs){
     
     let jresultado = res_logs.rows[0].jresultado;
 
-    //  Creación de un nuevo archivo en el que se guardaran los logs
-    globales.crearJSon('logs',JSON.stringify(jresultado))
 
     return jresultado;
 }
 
-async function logEnviado(json_logs){
+async function logEnviado(jsonLogEnviado){
     
     //si el estado esta vacio, mandara los registros del dia actual
-    let res_logs = await conexion.query("SELECT * FROM log_enviado('"+JSON.stringify(json_logs)+"');");
+    let res_logs = await conexion.query("SELECT * FROM log_enviado('"+JSON.stringify(jsonLogEnviado)+"');");
     
     let jresultado = res_logs.rows[0].jresultado;
 
-    //  Creación de un nuevo archivo en el que se guardaran los logs
-    globales.crearJSon('logs',JSON.stringify(jresultado))
+
+    return jresultado;
+}
+
+async function registraMensajesNoEnviados(json_logs){
+    
+    //si el estado esta vacio, mandara los registros del dia actual
+    let res_logs = await conexion.query("SELECT * FROM registra_mensajes_no_enviados('"+JSON.stringify(json_logs)+"');");
+    
+    let jresultado = res_logs.rows[0].jresultado;
 
     return jresultado;
 }
@@ -89,12 +93,14 @@ const TipusMensajeria = {
 const Service = 'gmail';
 
 class Rechazado{
-    constructor(tipo, destino, log_cod, usm_cod, razon){
+    constructor(tipo, destino, log_cod, usm_cod, usuario, razon){
         this.tipo = tipo;
         this.destino = destino;
         this.log_cod = log_cod;
         this.usm_cod = usm_cod;
+        this.usuario = usuario
         this.razon = razon;
+        this.emparejado = false;
     }
 
     mismoLogUsuario(rechazado){
@@ -103,70 +109,95 @@ class Rechazado{
     }
 }
 
+class Consulta{
+
+    constructor(usm_cod, log_cod, que){
+        this.usuario = usm_cod,
+        this.log_id = log_cod,
+        this.sin_enviar = '{'+que+'}'
+    }
+
+    add(tipo){
+        this.sin_enviar = this.sin_enviar.replace('}', ','+tipo+'}');
+    }
+}
+
 class Buff{
     
     #total = 0;
+    #finaliza = false;
+    #tiposRechazados = {};
 
     constructor(){
-        this.finaliza = false;
-        this.tiposRechazados = {};
         for (const tipus in TipusMensajeria) {
-            this.tiposRechazados[tipus]=[];
-            this.#total ++;
+            this.#tiposRechazados[tipus]=[];
         }
+        //globales.msg(this.#tiposRechazados)
     
     }
 
-    async inicia(){
-        var usuLogs = {logs:[], usuarios:[]};
-        usuarios_mensajeria().then(async (usuarios)=>{
+    inicia(){
+        // Estas funciones son promesas por tanto el hilo principal
+        // seguirá su camino aunque no hayan finalizado
+        usuarios_mensajeria().then((usuarios)=>{
             // ya tengo los usuarios
-            usuLogs.usuarios = usuarios;
 
             // ahora los logs
-            usuLogs.logs = await ver_logs({ desde: Desde, estado: Estados.ko });
+            ver_logs({ desde: Desde, estado: Estados.ko, enviado:false }).then((logs)=>{
 
-            
-        }).then(()=>{
-            // El log[0] contiene cod_error, por tanto si hay error sólo tenemos un
-            // registro y no se envia nada
-            usuLogs.logs.forEach((log, i) => {
-                if(i==1){
-                    var mensa = estructuraMensaje(log);
-                    // this.enviaEmailATodos(mensa, usuLogs.usuarios, log);
+                // Ya podemos enviar
+                // El log[0] contiene cod_error, por tanto si hay error sólo tenemos un
+                // registro y no se envia nada
+                logs.forEach((log, i) => {
+                    if(i>0){
+                        var mensa = estructuraMensaje(log);
+                        this.enviaEmailATodos(mensa, usuarios, log);
 
-                    usuLogs.usuarios.forEach((usuario, k) => {
-                        if(k==3){
-                            //! Descomenta la siguiente línea
-                            this.enviaMensajeUsuario(mensa, usuario, log);
-                        }
-                        
-                    });
-                }
-                
+                        usuarios.forEach((usuario, k) => {
+                            if(k>0){
+                                //! Descomenta la siguiente línea
+                                this.enviaMensajeUsuario(mensa, usuario, log);
+                            }
+                            
+                        });
+                    }
+                    
+                });
             });
+
             
         });
     }
 
     enviaEmailATodos(mensa, usuarios, log){
-        enviaEmailUsuarios(mensa, usuarios, log)
-            .then((rechazados)=>{
-                this.tiposRechazados[TipusMensajeria.email] = rechazados
-                this.gestionaRechazados();
-            });
+        this.#total++;
+        enviaEmailUsuarios(mensa, usuarios, log, (function (rechazados){
+            //globales.msg('Te he encontrado: '+rechazados)
+            // añadimos todo el array de los rechazado
+            //globales.msg(rechazados)
+
+            this.#tiposRechazados[TipusMensajeria.email].push(...rechazados);
+            this.gestionaRechazados();
+
+        }).bind(this));
     }
 
     enviaMensajeUsuario(mensaje, usuario, log){
+        // El tipo de mensajes de correo electrónico "email"
+        // va por otro camino
         for (const tipus in usuario.mensajeria) {
-            if(tipus==TipusMensajeria.email || tipus=='emailo'){
+            if(tipus==TipusMensajeria.email){
                 continue;
             }
-
+            // contador de envios
             this.#total++;
             Mensajerias[tipus](mensaje, usuario, log).then((rechazados)=>{
-                this.tiposRechazados[tipus].push(rechazados);
-                globales.msg(rechazados);
+                if(rechazados){
+                    //globales.msg(rechazados);
+                    this.#tiposRechazados[tipus].push(rechazados);
+                }
+
+            }).finally(()=>{
                 this.gestionaRechazados();
             });
         }
@@ -174,84 +205,93 @@ class Buff{
 
     gestionaRechazados(){
         this.#total--;
+        //globales.msg('descontando: '+(this.#total==0));
         if(this.#total==0){
+            globales.msg('dentro');
+            var consultas = [];
+            
+            //Recorremos los distintos tipos de rechazados
+            var grupoRechazados = this.#tiposRechazados;
+            for (const tipo in grupoRechazados) {
+                var elementos = grupoRechazados[tipo];
+                globales.msg(elementos);
+
+                globales.msg(elementos.length)
+                // por cada rechazado
+                elementos.forEach(rechazado =>{
+                    globales.msg(rechazado);
+
+                    // miramos si existe una consulta
+                    let consulta = consultas.find( quien =>{
+                        return quien.usuario == rechazado.usm_cod && quien.log_id == rechazado.log_cod
+                    });
+                    //globales.msg('--> '+consulta);
+
+                    if(consulta){
+
+                        // Añadimos el tipo rechazado
+                        consulta.add(rechazado.tipo);
+                    }else{
+                        globales.msg(rechazado)
+                        // Creamos la consulta
+                        consultas.push(
+                            new Consulta(
+                                rechazado.usm_cod, rechazado.log_cod, rechazado.tipo
+                            )
+                        );
+
+                    }
+                });
+            }
+
+            // ahora Pasamos la matriz de consultas cadena JSON
+            registraMensajesNoEnviados(consultas)
+            //globales.msg(JSON.stringify(consultas));
 
         }
     }
 }
 
-async function enviaEmailUsuarios(mensaje, usuarios, log){
+function enviaEmailUsuarios(mensaje, usuarios, log, alFinalizar){
+
+    Mensajerias[TipusMensajeria.email](mensaje, usuarios, log, alFinalizar);
+}
+
+
+function envia(){
+    
+    var buff = new Buff();
+    try{
+        buff.inicia();
+    }catch(e){
+        globales.msg('Catch Envia ----------');
+        globales.msg(e);
+        //! TODO crear el envío a los usuarios
+        //! administradores }
+    }
+}
+
+function mandaCorreos(mensaje, usuarios, log, alFinalizar){
 
     var mailList = [];
-    var mailUsuarioCod = {}
-    usuarios.forEach(usuario => {
+    var mailUsuario = {}
+    for(var i = 1; i<usuarios.length; i++){
+        var usuario = usuarios[i];
         var email = usuario.mensajeria.email;
         if(email){
             mailList.push(email);
-            mailUsuarioCod[email] = usuario.cod;
+            mailUsuario[email] = usuario;
+            //globales.msg(mailUsuario[email])
         }
-    });
+    }
 
     if(mailList.length==0){
         return mailList;
     }
 
-    Mensajerias[TipusMensajeria.email](mensaje, mailList, log,
-        function(error, info){
-            
-            if (error) {
-    
-                //! Enviar este error?
-                //! no deberia dar error
-                globales.msg('Error node mailer -------');
-                globales.msg(error);
-                return[];
-            } else {
-                // El mensaje se ha enviado
-                var lgEnviado = {cod:log.lg_cod, fecha: new Date(), enviado:true};
-                
-                // Marcamos en la BBDD los mensajes enviado
-                logEnviado(lgEnviado).catch((e)=>{
-                    //! Enviar este error?
-                    //! no deberia dar error
-                    globales.msg(e)
-                });
-                var rechazados = info.rejectedErrors;
-                for (var i=0; i<rechazados.length; i++){
-                    var rechazado = rechazados[i];
-                    var email = info.rejected;
-                    rechazados.push(
-                        new Rechazado(
-                            TipusMensajeria.email,
-                            email,
-                            log.lg_cod,
-                            mailUsuarioCod[email],
-                            rechazado.response)
-                    );
-                }
-                return rechazados;
-            }
-    });
-}
-
-
-function envia(){
-
-    var buff = new Buff();
-    buff.inicia().catch((e)=>{
-        globales.msg('Catch Envia ----------');
-        globales.msg(e);
-        //! TODO crear el envío a los usuarios
-        //! administradores 
-    });
-    
-
-}
-
-async function mandaCorreos(mensaje, mailList, log, alFinalizar){
     //Creamos el objeto de que mandara el correo y elegiremos el servicio que queramos
     var transporter = nodemailer.createTransport({
-        // imortante 
+        // importante 
         service: Service,
         auth: {
             user: usuCorreo.user,
@@ -262,45 +302,97 @@ async function mandaCorreos(mensaje, mailList, log, alFinalizar){
     //  Cuerpo del mensaje enviante
     var mailOptions = {
         from: usuCorreo.user, // Recoge el usuario escrito en el archivo json
-        to: mailList, //  Se lo manda a la/s personas que esten en el listado de correos !CAMBIAR
-        subject: log.acc_nombre+': '+log.lg_estado, // El asunto sera el nombre del error del servidor
+        to: mailList, //  Se lo manda a la/s personas que están en el listado de correos 
+        subject: log.acc_nombre+': '+log.lg_estado, // El asunto será el nombre la acción 
         text: mensaje// El mensaje sera toda la estructura del error
     };
     
     //  Comando para mandar el correo junto con mailOptions
     transporter.sendMail(mailOptions, function(error, info){
-        alFinalizar(error, info)
         transporter.close();
-      });
+
+        // El mensaje se ha enviado
+        var lgEnviado = {cod:log.lg_cod, fecha: new Date(), enviado:true};
+        
+        // Marcamos en la BBDD los mensajes enviado
+        logEnviado(lgEnviado).catch((e)=>{
+            //! Enviar este error?
+            //! no deberia dar error
+        });
+
+
+        // Pero puede que no a todos o a ninguno
+        // pero esto se deja constancia en la tabla
+        // de los mensajes no enviados
+
+        // Objetos de errores en el envio
+        var errores = [];
+        var rejected = [];
+
+        // Almacenamos los rechazados
+        var rechazados = [];
+
+        if (error) {
+            // No se ha enviado a nadie
+            errores = error.rejectedErrors;
+            rejected = error.rejected;
+        } else {
+            // Se ha enviado a los destinatarios el correo
+            // pero puede que no a todos
+            errores = info.rejectedErrors;
+            rejected = info.rejected;
+        }
+
+        // Montamos la estructura de los envios rechazados
+        for (var i=0; i<errores.length; i++){
+            var rechazado = errores[i];
+            var email = rejected[i];
+            var usuario = mailUsuario[email];
+            rechazados.push(
+                new Rechazado(
+                    TipusMensajeria.email,
+                    email,
+                    log.lg_cod,
+                    usuario.cod,
+                    usuario.usuario,
+                    rechazado.response
+                )
+            );
+        }
+
+        if(alFinalizar){
+            //Avisamos de que hemos finalizado
+            alFinalizar(rechazados);
+        }
+    });
 }
 
+//  Token del bot
+const token = tokenBot.token;
+
+// Create a bot that uses 'polling' to fetch new updates
+const bot = new TelegramBot(token, {polling: true});
     
 async function botTelegram(mensaje, usuario, log){
-    if(usuario.usuario!='joan')
-        return;
 
-    //  Token del bot
-    const token = tokenBot.token;
-
-    // Create a bot that uses 'polling' to fetch new updates
-    const bot = new TelegramBot(token, {polling: true});
-    
+    //globales.msg('esoty enviando');
     //  Manda un mensaje automático de los errores del dia actual
     var chatId = usuario.mensajeria.telegram;
         
+        //globales.msg(usuario.usuario)
     return bot.sendMessage(chatId+1,mensaje).then((x)=>{
         globales.msg('Telegram enviado ---------');
         globales.msg(x)
-        return [];
+        return false;
     }).catch((e)=>{
-        return [ 
-            new Rechazado(
+        return new Rechazado(
                 TipusMensajeria.telegram,
                 chatId,
                 log.lg_cod,
                 usuario.cod,
-                e.toString())
-        ];
+                usuario.usuario,
+                e.toString()
+            );
     })
 }
 
---
+++ Server_Nodejs/comandos/telegram.js
@@ -0,0 +1,4 @@
+//  Funciones generales del programa
+const globales = require('./globales');
+const tokenBot = require('../config/tokenBot.json');
+const TelegramBot = require('node-telegram-bot-api');
